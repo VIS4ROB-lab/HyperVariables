0c0c054bd53920b0bebeef94cb2af391cca99371
David Hug <david.hug@mavt.ethz.ch>
2022-07-21 14:20:45 +0200

    Minor change to composite variable

--- include/hyper/variables/composite.hpp
+++ include/hyper/variables/composite.hpp
@@ -15,60 +15,56 @@ namespace hyper {
 template <typename TScalar>
 class CompositeVariable {
  public:
+  using Size = std::size_t;
   using Variable = AbstractVariable<TScalar>;
   using Variables = std::vector<std::unique_ptr<Variable>>;
-  using Size = typename Variables::size_type;
 
-  /// Deleted default constructor.
-  CompositeVariable() = delete;
+  /// Default constructor.
+  CompositeVariable() = default;
 
-  /// Default destructor.
-  virtual ~CompositeVariable() = default;
-
-  /// Retrieves the number of variables.
-  /// \return Number of variables.
-  [[nodiscard]] auto numVariables() const -> Size {
-    return variables_.size();
-  }
+  /// Constructor from number of variables.
+  /// \param num_variables Number of variables.
+  explicit CompositeVariable(const Size& num_variables)
+      : variables_(num_variables) {}
 
   /// Variables accessor.
-  /// \return variables.
+  /// \return Variables.
   [[nodiscard]] auto variables() const -> const Variables& {
     return variables_;
   }
 
-  /// Variable modifier.
+  /// Variables modifier.
+  /// \return Variables.
+  [[nodiscard]] auto variables() -> Variables& {
+    return const_cast<Variables&>(std::as_const(*this).variables());
+  }
+
+  /// Variable accessor.
   /// \param index Variable index.
   /// \return Variable.
-  [[nodiscard]] auto variable(const Size index) const -> Variable& {
+  [[nodiscard]] auto variable(const Size& index) const -> Variable& {
     DCHECK_LT(index, variables_.size());
     DCHECK(variables_[index] != nullptr);
     return *variables_[index];
   }
 
+  /// Sets a variable at index.
+  /// \param index Variable index.
+  /// \param variable Input variable.
+  auto setVariable(const Size& index, std::unique_ptr<Variable>&& variable) -> void {
+    DCHECK_LT(index, variables_.size());
+    variables_[index] = std::move(variable);
+  }
+
   /// Memory block collector.
   /// \return Memory blocks.
-  [[nodiscard]] virtual auto memoryBlocks() const -> MemoryBlocks<TScalar> {
+  [[nodiscard]] auto memoryBlocks() const -> MemoryBlocks<TScalar> {
     MemoryBlocks<TScalar> memory_blocks;
     memory_blocks.reserve(variables_.size());
     for (const auto& variable : variables_) {
       memory_blocks.template emplace_back(variable->memory());
     }
     return memory_blocks;
-  }
-
- protected:
-  /// Constructor from number of variables.
-  /// \param num_variables Number of variables.
-  explicit CompositeVariable(int num_variables)
-      : variables_(num_variables) {}
-
-  /// Sets a variable at index.
-  /// \param index Variable index.
-  /// \param variable Input variable.
-  auto setVariable(const Size index, std::unique_ptr<Variable>&& variable) -> void {
-    DCHECK_LT(index, variables_.size());
-    variables_[index] = std::move(variable);
   }
 
  private:
