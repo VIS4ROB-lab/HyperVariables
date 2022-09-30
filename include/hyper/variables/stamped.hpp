7650d026bc102f40accf83b6203e15fed07538cd
David Hug <david.hug@mavt.ethz.ch>
2022-09-30 18:52:54 +0200

    Minor corrections

--- include/hyper/variables/stamped.hpp
+++ include/hyper/variables/stamped.hpp
@@ -31,23 +31,25 @@ class StampedBase
     : public Traits<TDerived>::Base,
       public AbstractStamped<typename Traits<TDerived>::ScalarWithConstIfNotLvalue> {
  public:
+  // Definitions.
   using Scalar = typename Traits<TDerived>::Scalar;
   using ScalarWithConstIfNotLvalue = typename Traits<TDerived>::ScalarWithConstIfNotLvalue;
+  using DynamicVectorWithConstIfNotLvalue = std::conditional_t<std::is_const_v<ScalarWithConstIfNotLvalue>, const DynamicVector<Scalar>, DynamicVector<Scalar>>;
   using Base = typename Traits<TDerived>::Base;
   using Base::Base;
 
   HYPER_INHERIT_ASSIGNMENT_OPERATORS(StampedBase)
 
-  /// Memory accessor.
-  /// \return Memory block.
-  [[nodiscard]] auto memory() const -> MemoryBlock<const Scalar> final {
-    return {this->data(), this->size()};
+  /// Map as Eigen vector.
+  /// \return Vector.
+  auto asVector() const -> Eigen::Map<const DynamicVector<Scalar>> final {
+    return {this->data(), this->size(), 1};
   }
 
-  /// Memory modifier.
-  /// \return Memory block.
-  [[nodiscard]] auto memory() -> MemoryBlock<ScalarWithConstIfNotLvalue> final {
-    return {this->data(), this->size()};
+  /// Map as Eigen vector.
+  /// \return Vector.
+  auto asVector() -> Eigen::Map<DynamicVectorWithConstIfNotLvalue> final {
+    return {this->data(), this->size(), 1};
   }
 
   /// Stamp accessor.
