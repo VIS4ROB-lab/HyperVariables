5e2eda13170093ea675137b7994ddee1ad1e69d9
David Hug <david.hug@mavt.ethz.ch>
2022-12-12 12:10:41 +0100

    Added covariance

--- include/hyper/variables/definitions/residual.hpp
+++ include/hyper/variables/definitions/residual.hpp
@@ -7,21 +7,13 @@
 
 namespace hyper {
 
-// clang-format off
-
-constexpr auto DefaultResidualStorageOption(const int rows, const int cols) -> int {
-  return Eigen::AutoAlign | ((rows == 1 && cols != 1) ? Eigen::RowMajor : ((cols == 1 && rows != 1) ? Eigen::ColMajor : EIGEN_DEFAULT_MATRIX_STORAGE_ORDER_OPTION)); // NOLINT
-}
-
-// clang-format on
-
-template <typename TScalar, int TNumRows, int TOptions = DefaultResidualStorageOption(TNumRows, 1)>
+template <typename TScalar, int TNumRows, int TOptions = DefaultVectorStorageOption(TNumRows, 1)>
 using Residual = Vector<TScalar, TNumRows, TOptions>;
 
-template <typename TDerived, int TOptions = DefaultResidualStorageOption(TDerived::SizeAtCompileTime, 1)>
+template <typename TDerived, int TOptions = DefaultVectorStorageOption(TDerived::SizeAtCompileTime, 1)>
 using ResidualN = VectorN<TDerived, TOptions>;
 
-template <typename TScalar, int TOptions = DefaultResidualStorageOption(Eigen::Dynamic, 1)>
+template <typename TScalar, int TOptions = DefaultVectorStorageOption(Eigen::Dynamic, 1)>
 using ResidualX = VectorX<TScalar, TOptions>;
 
 } // namespace hyper
