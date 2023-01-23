8c9a570495c7f21d0a78f1c13f0cf9d2b0e0d168
David Hug <david.hug@mavt.ethz.ch>
2023-01-23 16:22:15 +0100

    Added manifold wrapper

---
+++ include/hyper/manifolds/ceres/wrapper.hpp
@@ -0,0 +1,44 @@
+/// This file is subject to the terms and conditions defined in
+/// the 'LICENSE' file, which is part of this repository.
+
+#pragma once
+
+#include <ceres/manifold.h>
+
+namespace hyper::manifolds {
+
+/// @class Manifold wrapper for Ceres.
+/// This wrapper is required due to some
+/// manifolds being marked as final within
+/// Ceres, rendering them non-inheritable.
+/// We refer to the Ceres documentation
+/// for all inherited and overloaded
+/// functions in this class.
+class ManifoldWrapper : public ceres::Manifold {
+ public:
+  using Scalar = double;  // Ceres scalar type.
+
+  [[nodiscard]] auto AmbientSize() const -> int final;
+
+  [[nodiscard]] auto TangentSize() const -> int final;
+
+  auto Plus(const Scalar* x, const Scalar* delta, Scalar* x_plus_delta) const -> bool final;
+
+  auto PlusJacobian(const Scalar* x, Scalar* jacobian) const -> bool final;
+
+  auto RightMultiplyByPlusJacobian(const Scalar* x, const int num_rows, const Scalar* ambient_matrix, Scalar* tangent_matrix) const -> bool final;  // NOLINT
+
+  auto Minus(const Scalar* y, const Scalar* x, Scalar* y_minus_x) const -> bool final;
+
+  auto MinusJacobian(const Scalar* x, Scalar* jacobian) const -> bool final;
+
+ protected:
+  /// Protected constructor from input manifold.
+  /// \param manifold Input manifold.
+  explicit ManifoldWrapper(std::unique_ptr<ceres::Manifold>&& manifold);
+
+ private:
+  std::unique_ptr<ceres::Manifold> manifold_;  ///< Manifold.
+};
+
+}  // namespace hyper::manifolds
---
+++ internal/hyper/manifolds/ceres/wrapper.cpp
@@ -0,0 +1,38 @@
+/// This file is subject to the terms and conditions defined in
+/// the 'LICENSE' file, which is part of this repository.
+
+#include "hyper/manifolds/ceres/wrapper.hpp"
+
+namespace hyper::manifolds {
+
+auto ManifoldWrapper::AmbientSize() const -> int {
+  return manifold_->AmbientSize();
+}
+
+auto ManifoldWrapper::TangentSize() const -> int {
+  return manifold_->TangentSize();
+}
+
+auto ManifoldWrapper::Plus(const Scalar* x, const Scalar* delta, Scalar* x_plus_delta) const -> bool {
+  return manifold_->Plus(x, delta, x_plus_delta);
+}
+
+auto ManifoldWrapper::PlusJacobian(const Scalar* x, Scalar* jacobian) const -> bool {
+  return manifold_->PlusJacobian(x, jacobian);
+}
+
+auto ManifoldWrapper::RightMultiplyByPlusJacobian(const Scalar* x, const int num_rows, const Scalar* ambient_matrix, Scalar* tangent_matrix) const -> bool {
+  return manifold_->RightMultiplyByPlusJacobian(x, num_rows, ambient_matrix, tangent_matrix);
+}
+
+auto ManifoldWrapper::Minus(const Scalar* y, const Scalar* x, Scalar* y_minus_x) const -> bool {
+  return manifold_->Minus(y, x, y_minus_x);
+}
+
+auto ManifoldWrapper::MinusJacobian(const Scalar* x, Scalar* jacobian) const -> bool {
+  return manifold_->MinusJacobian(x, jacobian);
+}
+
+ManifoldWrapper::ManifoldWrapper(std::unique_ptr<ceres::Manifold>&& manifold) : manifold_{std::move(manifold)} {}
+
+}  // namespace hyper::manifolds
